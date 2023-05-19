434c6f5aa04c1f60e4b25ff72b0203c5d2c2d097
eyagueddes <eyagueddes1@gmail.com>
2023-05-19 23:27:46 +0100

    first commi

---
+++ src/main/java/com/sip/ams_back/AmsBackApplication.java
@@ -0,0 +1,15 @@
+package com.sip.ams_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsBackApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AmsBackApplication.class, args);
+        System.out.println("full stack project begin");
+    }
+
+}
